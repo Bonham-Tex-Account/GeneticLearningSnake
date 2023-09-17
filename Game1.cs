af1200395b7c88037254439b668d8c2525c9fc5c
Tex Bonham <texwasbad@gmail.com>
2023-09-17 13:14:47 -0700

    Load on to GitHub

---
+++ Game1.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using System;
+
+namespace SnakeGeneticLearning
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        int countofsnake = 15;
+        Generation gen;
+        InfoBloc[] bloclist;
+        double BestSnake = 1;
+        TimeSpan timer = new TimeSpan();
+        double gamespeed = 1;
+        double lastchange = 0;
+        Random random;
+        int turncounter = 0;
+        SpriteFont sf;
+        int gencounter;
+        double[] computemap;
+        KeyboardState state;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 10 * countofsnake + 1;
+            _graphics.PreferredBackBufferHeight = 10 * countofsnake + 1;
+            _graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            computemap = new double[countofsnake * countofsnake];
+            sf = Content.Load<SpriteFont>("File");
+            random = new Random();
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            bloclist = new InfoBloc[countofsnake];
+            gen = new Generation(countofsnake);
+
+            // TODO: use this.Content to load your game content here
+            for (int i = 0; i < countofsnake; i++)
+            {
+                bloclist[i] = new InfoBloc(new Snake(5*countofsnake, 5*countofsnake), new Individual(new int[] { computemap.Length * 2 / 12 + 4, 4 }, computemap.Length, random), 0,countofsnake);
+                bloclist[i].snake.Food = new FoodPiece();
+                bloclist[i].snake.Food.xposition = 5*countofsnake;
+                bloclist[i].snake.Food.yposition = 5*countofsnake;
+                bloclist[i].pastpos.X = 5*countofsnake;
+                bloclist[i].pastpos.Y = 5*countofsnake;
+               
+            }
+           
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            for (int l = 0; l < gamespeed; l++)
+            {
+
+                timer = timer.Add(new TimeSpan(0, 0, 0, 0, 1));
+                turncounter++;
+                bool allive = false;
+                KeyboardState keys = Keyboard.GetState();
+                if (keys.IsKeyDown(Keys.Up) && (timer.TotalMilliseconds - lastchange) > 500)
+                {
+                    gamespeed++;
+                    lastchange = timer.TotalMilliseconds;
+                }
+                if (keys.IsKeyDown(Keys.Down) && (timer.TotalMilliseconds - lastchange) > 500)
+                {
+                    gamespeed--;
+                    lastchange = timer.TotalMilliseconds;
+                }
+                if (keys.IsKeyDown(Keys.K) && !state.IsKeyDown(Keys.K))
+                {
+                    foreach (var i in bloclist)
+                    {
+                        i.snake.lifestatus = false;
+                    }
+                }
+                if (keys.IsKeyDown(Keys.C) && !state.IsKeyDown(Keys.C))
+                {
+                    ;
+                }
+                // TODO: Add your update logic here
+                foreach (var i in bloclist)
+                {
+                    
+                    if (i.snake.lifestatus)
+                    {
+                      
+                        for (int j = 0; j < countofsnake; j++)
+                        {
+                            for (int k = 0; k < countofsnake; k++)
+                            {
+                                computemap[j * countofsnake + k] = i.map[j][k];
+                            }
+                        }
+                        double[] directionoutput = i.info.network.Compute(computemap);
+                        /* if (Math.Abs(directionoutput[0]) > Math.Abs(directionoutput[1]))
+                         {
+                             if (directionoutput[0] > 0)
+                             {
+                                 if (i.snake.direction != 3)
+                                 {
+                                     i.snake.direction = 1;
+                                 }
+                                 else
+                                 {
+                                     ;
+                                 }
+
+                             }
+                             else
+                             {
+                                 if (i.snake.direction != 1)
+                                 {
+                                     i.snake.direction = 3;
+                                 }
+                                 else
+                                 {
+                                     ;
+                                 }
+                             }
+                         }
+                         else
+                         {
+                             if (directionoutput[1] > 0)
+                             {
+                                 if (i.snake.direction != 0)
+                                 {
+                                     i.snake.direction = 2;
+                                 }
+                                 else
+                                 {
+                                     ;
+                                 }
+                             }
+                             else
+                             {
+                                 if (i.snake.direction != 2)
+                                 {
+                                     i.snake.direction = 0;
+                                 }
+                                 else
+                                 {
+                                     ;
+                                 }
+                             }
+                         }*/
+                        int greatest = 0;
+                        double best = 0;
+                        for (int k = 0; k < directionoutput.Length; k++)
+                        {
+                            if (directionoutput[k] > best)
+                            {
+                                best = directionoutput[k];
+                                greatest = k;
+                            }
+                        }
+                        if ((greatest == 1 && i.snake.lastdir == 3) || (greatest == 3 && i.snake.lastdir == 1) || (greatest == 0 && i.snake.lastdir == 2) || (greatest == 2 && i.snake.lastdir == 0))
+                        {
+
+                            i.snake.direction = i.snake.lastdir;
+                        }
+                        else
+                        {
+                            i.snake.direction = greatest;
+                        }
+
+
+
+                        i.snake.Move(random,turncounter,countofsnake);
+                        if (i.snake.lifestatus)
+                        {
+                            i.fitnessval = timer.TotalMilliseconds / 1000 + i.snake.score * 100;
+                        }
+
+                        if (i.snake.lifestatus)
+                        {
+                            i.info.fitness = i.fitnessval;
+                        }
+
+                        i.currpos.X = (float)i.snake.Head.xposition;
+                        i.currpos.Y = (float)i.snake.Head.yposition;
+                        if(i.snake.lastgrow+500<turncounter)
+                        {
+                            i.snake.lifestatus = false;
+                            i.fitnessval = 0;
+                        }
+                        if (turncounter % 100 == 0 && turncounter > 1)
+                        {
+                            if (i.snake.lifestatus && (!(Math.Abs(i.currpos.X - i.pastpos.X) > 50) && !(Math.Abs(i.currpos.Y - i.pastpos.Y) > 50)))
+                            {
+                                i.snake.lifestatus = false;
+                                i.fitnessval = 0;
+                            }
+                            else
+                            {
+                                i.pastpos.X = i.currpos.X;
+                                i.pastpos.Y = i.currpos.Y;
+                            }
+                        }
+                        if (i.fitnessval > BestSnake)
+                        {
+                            BestSnake = i.fitnessval;
+                        }
+                        if (i.snake.lifestatus)
+                        {
+                            allive = true;
+                            i.MapUpdate(countofsnake);
+                        }
+                       
+                    }
+                }
+                if (!allive)
+                {
+                    GenReset();
+                }
+               
+                base.Update(gameTime);
+                state = keys;
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Begin();
+            GraphicsDevice.Clear(Color.Black);
+            foreach (var i in bloclist)
+            {
+                i.snake.DrawSnake(i.snake.Head, _spriteBatch, i.fitnessval / BestSnake);
+                if (i.snake.lifestatus)
+                {
+                    _spriteBatch.DrawRectangle((float)i.snake.Food.xposition, (float)i.snake.Food.yposition, 10, 10, Color.Red, 1, 0);
+                }
+
+            }
+            _spriteBatch.DrawString(sf, gencounter.ToString(), new Vector2((int)(4.5*countofsnake), (int)(4.7*countofsnake)), Color.White);
+            // TODO: Add your drawing code here
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        public void GenReset()
+        {
+
+            for (int i = 0; i < countofsnake; i++)
+            {
+                gen.population[i] = bloclist[i].info;
+
+            }
+            gen.GenerationTraining(random, 0.1);
+            for (int i = 0; i < countofsnake; i++)
+            {
+                bloclist[i].info = gen.population[i];
+            }
+            timer = timer.Subtract(timer);
+            Console.WriteLine(BestSnake);
+            BestSnake = 0;
+            foreach (var i in bloclist)
+            {
+                i.fitnessval = 0;
+                i.info.fitness = 0;
+                i.snake.Head.xposition = 5*countofsnake;
+                i.snake.Head.yposition = 5*countofsnake;
+                i.snake.lifestatus = true;
+                i.snake.score = 0;
+                i.snake.Tail = i.snake.Head;
+                i.snake.Food.xposition = 5*countofsnake;
+                i.snake.Food.yposition = 5*countofsnake;
+                i.pastpos.X = 5*countofsnake;
+                i.pastpos.Y = 5*countofsnake;
+                turncounter = 0;
+
+
+            }
+            gencounter++;
+        }
+    }
+
+}
---
+++ NeuralNet.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGeneticLearning
+{
+    public static class Extensions
+    {
+        public static double NextDouble(this Random random, double min, double max)
+        {
+            return random.NextDouble() * (max - min) + min;
+        }
+    }
+    class Generation
+    {
+
+        public Individual[] population;
+        public Generation(int popcount)
+        {
+            population = new Individual[popcount];
+        }
+        public void InitializePopulation(int[] NeuronsPerLayer, int Inputs, Random random)
+        {
+            for (int i = 0; i < population.Length; i++)
+            {
+                population[i] = new Individual(NeuronsPerLayer, Inputs, random);
+            }
+        }
+        void Crossover(NeuralNetwork winner, NeuralNetwork loser, Random random)
+        {
+            for (int i = 0; i < winner.layers.Length; i++)
+            {
+                //References to the Layers
+                Layer winLayer = winner.layers[i];
+                Layer childLayer = loser.layers[i];
+
+                int cutPoint = random.Next(winLayer.Neurons.Length); //calculate a cut point for the layer
+                bool flip = random.Next(2) == 0; //randomly decide which side of the cut point will come from winner
+
+                //Either copy from 0->cutPoint or cutPoint->Neurons.Length from the winner based on the flip variable
+                for (int j = (flip ? 0 : cutPoint); j < (flip ? cutPoint : winLayer.Neurons.Length); j++)
+                {
+                    //References to the Neurons
+                    Neuron winNeuron = winLayer.Neurons[j];
+                    Neuron childNeuron = childLayer.Neurons[j];
+
+                    //Copy the winners Weights and Bias into the loser/child neuron
+                    for (int k = 0; k < winNeuron.dendrites.Length; k++)
+                    {
+                        childNeuron.dendrites[0].Weight = winNeuron.dendrites[k].Weight;
+                    }
+                    childNeuron.bias = winNeuron.bias;
+                }
+            }
+
+        }
+        void Mutate(NeuralNetwork net, Random random, double mutationRate)
+        {
+            foreach (Layer layer in net.layers)
+            {
+                foreach (Neuron neuron in layer.Neurons)
+                {
+                    //Mutate the Weights
+                    for (int i = 0; i < neuron.dendrites.Length; i++)
+                    {
+                        if (random.NextDouble() < mutationRate)
+                        {
+                            if (random.Next(2) == 0)
+                            {
+                                neuron.dendrites[i].Weight *= (random.NextDouble() + 0.5); //scale weight
+                            }
+                            else
+                            {
+                                neuron.dendrites[i].Weight *= -1; //flip sign
+                            }
+                        }
+                    }
+
+                    //Mutate the Bias
+                    if (random.NextDouble() < mutationRate)
+                    {
+                        if (random.Next(2) == 0)
+                        {
+                            neuron.bias *= (random.NextDouble() + 0.5); //scale weight
+                        }
+                        else
+                        {
+                            neuron.bias *= -1; //flip sign
+                        }
+                    }
+                }
+            }
+        }
+
+        public void FitnessTest(double[] fitness)
+        {
+            for (int i = 0; i < population.Length; i++)
+            {
+                population[i].fitness = fitness[i];
+            }
+
+        }
+        //Trains Generation and returns best Individual
+        public Individual GenerationTraining(Random random, double mrate)
+        {
+           // FitnessTest(fitness);
+            Array.Sort(population, (a, b) => b.fitness.CompareTo(a.fitness));
+            //splits the population between top 10% and the rest
+            ;
+            int start = (int)(population.Length * .1);
+            int end = (int)(population.Length * .9);
+            // crosses over the midlle 80% with the top 10%
+            for (int i = start; i < end; i++)
+            {
+                Crossover(population[random.Next(start)].network, population[i].network, random);
+                Mutate(population[i].network, random, mrate);
+            }
+            ;
+            //Randomizes the worst 10%
+            for (int i = end; i < population.Length; i++)
+            {
+                population[i].network.Randomize(random);
+            }
+            return population[0];
+        }
+    }
+    class Individual
+    {
+        public double fitness;
+        public NeuralNetwork network;
+        public static double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Pow(Math.E, -x));
+        }
+        ErrorFunction errorfunc = new ErrorFunction((input, output) => (Math.Pow(input - output, 2)), (input, output) => (2 * (input - output)));
+        ActivationFunction acfunc = new ActivationFunction((input) => (Math.Tanh(input)), (input) => (0));
+        public Individual(int[] neuronperlayer, int inputsize, Random random)
+        {
+            network = new NeuralNetwork(acfunc, errorfunc, inputsize, neuronperlayer);
+            network.Randomize(random);
+
+        }
+    }
+    class Dendrite
+    {
+        public double weightupdate { get; set; }
+        public Neuron Previous { get; }
+        public Neuron Next { get; }
+        public double Weight { get; set; }
+
+        public Dendrite(Neuron previous, Neuron next, double weight)
+        {
+            Previous = previous;
+            Next = next;
+            Weight = weight;
+        }
+        public void ApplyUpdate()
+        {
+            Weight += weightupdate;
+            weightupdate = 0;
+        }
+        public double Compute()
+        {
+            return Previous.Output * Weight;
+        }
+    }
+    class Neuron
+    {
+        public double bias;
+        public double biasupdate;
+        public double delta { get; set; }
+
+        public Dendrite[] dendrites;
+        public double Output { get; set; }
+        public double Input { get; private set; }
+        public ActivationFunction Activation { get; set; }
+
+        public Neuron(ActivationFunction activation, Neuron[] previousNeurons)
+        {
+            Activation = activation;
+            dendrites = new Dendrite[previousNeurons.Length];
+            for (int i = 0; i < previousNeurons.Length; i++) dendrites[i] = new Dendrite(previousNeurons[i], null, 0);
+        }
+
+        public void Randomize(Random random)
+        {
+            bias = random.NextDouble(-7, 7);
+
+            foreach (var i in dendrites)
+            {
+                i.Weight = random.NextDouble(-7, 7);
+
+            }
+
+        }
+        public void ApplyUpdate()
+        {
+            foreach (var i in dendrites) i.ApplyUpdate();
+            bias += biasupdate;
+            biasupdate = 0;
+        }
+        public void backprop(double learningrate)
+        {
+            double biasderivative = Activation.Derivative(Input) * delta;
+            delta = 0;
+            biasupdate += biasderivative * -learningrate;
+            foreach (var i in dendrites)
+            {
+                i.weightupdate += -learningrate * biasderivative * i.Previous.Output;
+                ;
+            }
+        }
+        public double Compute(int layerindex)
+        {
+            Output = 0;
+            Input = bias;
+            foreach (var i in dendrites)
+            {
+                Input += i.Compute();
+            }
+            Output = Activation.Function(Input);
+            return Output;
+        }
+    }
+    class Layer
+    {
+        public Neuron[] Neurons { get; set; }
+        public double[] Outputs { get; set; }
+
+        public Layer(ActivationFunction activation, int neuronCount, Layer previousLayer)
+        {
+            Outputs = new double[neuronCount];
+            Neurons = new Neuron[neuronCount];
+            for (int i = 0; i < Neurons.Length; i++) Neurons[i] = new Neuron(activation, previousLayer.Neurons);
+        }
+        public void Randomize(Random random)
+        {
+            foreach (var i in Neurons) i.Randomize(random);
+        }
+        public void ApplyUpdate()
+        {
+            foreach (var i in Neurons) i.ApplyUpdate();
+
+        }
+        public void backprop(double learningrate)
+        {
+            foreach (var i in Neurons) i.backprop(learningrate);
+        }
+        public double[] Compute(int layerindex)
+        {
+
+            for (int i = 0; i < Outputs.Length; i++) Outputs[i] = Neurons[i].Compute(layerindex);
+            return Outputs;
+        }
+    }
+    class NeuralNetwork
+    {
+        public Layer[] layers;
+        ErrorFunction errorFunc;
+        Layer Inputlayer;
+
+        public NeuralNetwork(ActivationFunction activation, ErrorFunction ErrorFunc, int inputsize,
+        params int[] neuronsPerLayer)
+        {
+            Inputlayer = new Layer(activation, inputsize, new Layer(activation, 0, null));
+            errorFunc = ErrorFunc;
+            layers = new Layer[neuronsPerLayer.Length];
+            for (int i = 0; i < neuronsPerLayer.Length; i++)
+            {
+                if (i >= 1) layers[i] = new Layer(activation, neuronsPerLayer[i], layers[i - 1]);
+                else
+                {
+                    layers[i] = new Layer(activation, neuronsPerLayer[i], Inputlayer);
+                }
+            }
+        }
+        public void Randomize(Random random)
+        {
+            foreach (var i in layers) i.Randomize(random);
+        }
+        public void ApplyUpdate()
+        {
+            foreach (var i in layers) i.ApplyUpdate();
+        }
+        public void backprop(double learningrate, double[] desiredoutputs)
+        {
+            for (int i = layers.Length - 1; i >= 0; i--)
+            {
+                if (i == layers.Length - 1)
+                {
+                    for (int k = 0; k < layers[i].Neurons.Length; k++)
+                    {
+                        layers[i].Neurons[k].delta += errorFunc.Derivative(layers[i].Neurons[k].Compute(0), desiredoutputs[k]);
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < layers[i].Neurons.Length; k++)
+                    {
+                        foreach (var j in layers[i + 1].Neurons)
+                        {
+                            foreach (var l in j.dendrites)
+                            {
+                                layers[i].Neurons[k].delta += layers[i].Neurons[k].Activation.Derivative(layers[i].Neurons[k].Input) * j.delta * l.Weight;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            for (int i = layers.Length - 1; i >= 0; i--) layers[i].backprop(learningrate);
+        }
+        public double Train(double[][] inputs, double[][] desiredoutputs, double learningrate)
+        {
+            double averageerror = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                averageerror += GetError(inputs[i], desiredoutputs[i]);
+                backprop(learningrate, desiredoutputs[i]);
+            }
+            ApplyUpdate();
+            averageerror /= inputs.Length;
+            return averageerror;
+        }
+        public double[] Compute(double[] inputs)
+        {
+            Inputlayer.Outputs = inputs;
+            // Inputlayer.Neurons = new Neuron[inputs.Length];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                // Inputlayer.Neurons[i] = new Neuron(null, new Neuron[0]);
+                Inputlayer.Neurons[i].Output = inputs[i];
+
+            }
+            ;
+            for (int i = 0; i < layers.Length; i++)
+            {
+                if (i + 1 != layers.Length) layers[i].Compute(layers.Length - (i + 1));
+                else return layers[i].Compute(layers.Length - (i + 1));
+            }
+            return null;
+        }
+        public double GetError(double[] inputs, double[] desiredOutputs)
+        {
+            double error = 0;
+            double[] outputs = Compute(inputs);
+            for (int i = 0; i < desiredOutputs.Length; i++) error += errorFunc.Function(outputs[i], desiredOutputs[i]);
+            return error;
+        }
+    }
+    class ActivationFunction
+    {
+        Func<double, double> function;
+        Func<double, double> derivative;
+        public ActivationFunction(Func<double, double> Function, Func<double, double> Derivative)
+        {
+            if (Function == null || Derivative == null)
+            {
+                throw new NullReferenceException();
+            }
+            function = Function;
+            derivative = Derivative;
+        }
+
+        public double Function(double input)
+        {
+            return function(input);
+        }
+
+        public double Derivative(double input)
+        {
+            return derivative(input);
+        }
+    }
+    class ErrorFunction
+    {
+        Func<double, double, double> function;
+        Func<double, double, double> derivative;
+        public ErrorFunction(Func<double, double, double> Function, Func<double, double, double> Derivative)
+        {
+            if (Function == null || Derivative == null)
+            {
+                throw new NullReferenceException();
+            }
+            function = Function;
+            derivative = Derivative;
+        }
+
+        public double Function(double output, double desiredOutput)
+        {
+            return function(output, desiredOutput);
+        }
+        public double Derivative(double output, double desiredOutput)
+        {
+            return derivative(output, desiredOutput);
+        }
+    }
+}
